9f5428e41a1d6e8ad4f803dd09aae6b68c97d140
jusuf-el <58688886+jusuf-el@users.noreply.github.com>
2020-04-07 17:49:03 +0200

    Layout and indexes on orderable list

--- lib/components/SurveyCardYesNo/components/surveyCardComponents/orderComponents/choiceContainer.dart
+++ lib/components/SurveyCardYesNo/components/surveyCardComponents/orderComponents/choiceContainer.dart
@@ -1,10 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fillproject/components/answerLabelContainer.dart';
 import 'package:fillproject/components/constants/fontsConstants.dart';
 import 'package:fillproject/components/constants/myColor.dart';
-import 'package:fillproject/components/emptyCont.dart';
 import 'package:fillproject/globals.dart';
-import 'package:fillproject/localization/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -28,44 +25,40 @@ class ChoiceContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-            decoration: BoxDecoration(
-                border: Border.all(width: 1.0, color: MyColor().black),
-                borderRadius: BorderRadius.all(Radius.circular(33.5)),
-                color: MyColor().white),
-            width: ScreenUtil.instance.setWidth(350.0),
-            margin: EdgeInsets.only(top: ScreenUtil.instance.setWidth(15.0)),
-            child: ListTile(
-                leading: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 1.0, color: MyColor().black),
-                      borderRadius: BorderRadius.all(Radius.circular(100)),
-                      color: MyColor().black),
-                  height: ScreenUtil.instance.setHeight(58.0),
-                  width: ScreenUtil.instance.setWidth(58.0),
-                  child: Text(index,
-                      style: TextStyle(
-                          color: MyColor().white,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: arabic,
-                          fontStyle: FontStyle.normal,
-                          fontSize: ScreenUtil.instance.setSp(18.0))),
-                ),
-                title: Container(
-                  alignment: Alignment.centerLeft,
-                  child: Text(choice1,
-                      style: TextStyle(
-                          color: MyColor().black,
-                          fontWeight: FontWeight.w700,
-                          fontFamily: arabic,
-                          fontStyle: FontStyle.normal,
-                          fontSize: ScreenUtil.instance.setSp(18.0))),
-                ),
-                onTap: () => null)),
-      ],
-    );
+    return Container(
+        decoration: BoxDecoration(
+            border: Border.all(width: 1.0, color: MyColor().black),
+            borderRadius: BorderRadius.all(Radius.circular(33.5)),
+            color: MyColor().white),
+        width: ScreenUtil.instance.setWidth(350.0),
+        margin: isSummary ? EdgeInsets.only(top: ScreenUtil.instance.setWidth(15.0)) : null,
+        child: ListTile(
+            leading: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: MyColor().black),
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  color: MyColor().black),
+              height: ScreenUtil.instance.setHeight(58.0),
+              width: ScreenUtil.instance.setWidth(58.0),
+              child: Text(index,
+                  style: TextStyle(
+                      color: MyColor().white,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: arabic,
+                      fontStyle: FontStyle.normal,
+                      fontSize: ScreenUtil.instance.setSp(18.0))),
+            ),
+            title: Container(
+              alignment: Alignment.centerLeft,
+              child: Text(choice1,
+                  style: TextStyle(
+                      color: MyColor().black,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: arabic,
+                      fontStyle: FontStyle.normal,
+                      fontSize: ScreenUtil.instance.setSp(18.0))),
+            ),
+            onTap: () => null));
   }
 }
